c34956ad17561940591f405cd850a485cf8dab2a
bryan.gonzalez96.bg@gmail.com <bryan.gonzalez96.bg@gmail.com>
2018-12-10 10:31:09 -0600

    Project Started

---
+++ src/Components/Landing/Landing.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Landing.scss";
+
+const Landing = () => {
+  return (
+    <div className="Landing">
+      <h1>Landing</h1>
+    </div>
+  );
+};
+
+export default Landing;
