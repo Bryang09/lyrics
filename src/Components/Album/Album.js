14695f2cb4bef39dbce66513952f3427d7f5c003
bryan.gonzalez96.bg@gmail.com <bryan.gonzalez96.bg@gmail.com>
2018-12-18 10:07:47 -0600

    Artist Page Completed

--- src/Components/Album/Album.js
+++ src/Components/Album/Album.js
@@ -1,7 +1,27 @@
 import React, { Component } from "react";
 
+import { Request } from "../../Request";
+
+import axios from "axios";
+
 class Album extends Component {
+  state = {
+    tracks: []
+  };
+
+  componentWillMount = () => {
+    const album = this.props.match.params.id;
+    axios
+      .get(
+        `${Request}album.tracks.get?album_id=${album}&page=1&page_size=2&apikey=${
+          process.env.REACT_APP_KEY
+        }`
+      )
+      .then(res => this.setState({ tracks: res.data.message.body.track_list }))
+      .catch(err => console.log(err));
+  };
   render() {
+    console.log(this.props);
     return (
       <div className="Album">
         <h1>Album</h1>
--- src/Components/Artist/Albums/Album/Album.js
+++ src/Components/Artist/Albums/Album/Album.js
@@ -1,16 +1,37 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
+import "./Album.scss";
+
 const Album = props => {
   const albums = props.albums;
-  return (
-    <div className="album">
-      <div className="albumCover" />
-      <div className="albumInfo">
-        <h4>Album Name</h4>
-        <h4>Album Release </h4>
+
+  const Album = albums.map(res => {
+    const album = res.album;
+    return (
+      <div className="album" key={album.album_id}>
+        <div
+          className="albumCover"
+          style={{ backgroundImage: `url(${album.album_coverart_100x100})` }}
+        />
+        <div className="albumInfo">
+          <h4 style={{ fontWeight: 400, fontSize: "1.5em" }}>
+            <Link
+              to={{
+                pathname: `/album/${album.album_id}`
+              }}
+            >
+              {album.album_name}
+            </Link>
+          </h4>
+          <h4 style={{ fontWeight: 300 }}>{album.album_release_date}</h4>
+        </div>
       </div>
-    </div>
-  );
+    );
+  });
+
+  return <>{Album}</>;
 };
 
 export default Album;
